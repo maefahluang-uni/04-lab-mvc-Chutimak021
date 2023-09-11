494f6618c32c5b103905d151741c59d825190cf7
6531503021 <6531503021@lamduan.mfu.ac.th>
2023-09-11 20:37:02 +0700

    6531503021

--- src/main/java/th/mfu/Concert.java
+++ src/main/java/th/mfu/Concert.java
@@ -4,37 +4,59 @@ import java.util.Date;
 
 public class Concert {
 
-    //TODO: add attributes
-    private int id;
-    private String title;
-    private String performer;
-    private Date date;
-    private String location;
-    private String description;
+ //TODO: add attributes
+ private int id;
+ private String title;
+ private String performer;
+ private Date date;
+ private String description;
 
-    public Concert() {
-    }
-    public Concert(String title, String description) {
-        //TODO: set attributes
-        this.title = title;
-        this.description = description;
-    }
+ public Concert() {
+ }
+ public Concert(String title, String description) {
+ //TODO: set attributes
+ this.title = title;
+ this.description = description;
+ }
+
+ public String getTitle() {
+ //TODO: return title
+ return title;
+ }
+ public void setTitle(String title){
+ this.title = title;
+ }
+
+ public String getDescription() {
+ //TODO: return description
+ return description;
+ }
+ public void setDescriptione(String description){
+this.description = description;
+ }
+
+
+ //TODO: add getters and setters
+ public void setId(int id) {
+ this.id = id;
+ }
+ public int getId() {
+     return id;
+ }
  
-    public String getTitle() {
-        //TODO: return title
-        return title;
-    }
-    public String getDescription() {
-        //TODO: return description
-        return description;
-    }
+ public Date getDate() {
+ return date;
+ }
+ public void setDate(Date date) {
+ this.date = date;
+ }
 
-    //TODO: add getters and setters
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
+ 
+ public String getPerformer() {
+ return performer;
+ }
+ public void setPerformer(String performer) {
+ this.performer = performer;
+ }
 
 }
